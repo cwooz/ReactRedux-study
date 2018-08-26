bb726e363095d7b758b51ca6452b6341f8e24c07
chrisWoosley <woosley.christopher@gmail.com>
2018-08-26 19:16:14 -0400

    material-ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,64 @@
-import React, { Component } from 'react';
+// import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+// import Button from "@material-ui/core/Button";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
   }
+};
+
+function App(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            React-Redux
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Button variant="contained" color="primary">
+//         Hello World
+//       </Button>
+//     );
+//   }
+// }
